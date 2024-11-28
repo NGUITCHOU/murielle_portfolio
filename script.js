014ba53124e16f3f8565a4afbd69ecf28f645953
unknown <nguitchoumurielle@gmail.com>
2024-11-28 19:06:05 +0100

    add updated files to menu_dropdown

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const { textContent } = require("domutils");
+
 const mobileMenu = document.getElementById('mobile_menu');
 
 mobileMenu.addEventListener('click', () => {
@@ -13,37 +15,50 @@ const projects = [
     image: '#',
     title: 'Multi-Post Stories Gain+Glory',
     technologies: ['Ruby on Rails', 'CSS', 'Javascript', 'HTML'],
-    cardButton: '#',
+    description: 'An interactive gallery that showcase my project',
+    githublink: '#',
+    liveserver: '#',
   },
   {
     image: '#',
     title: 'Multi-Post Stories Gain+Glory',
     technologies: ['Ruby on Rails', 'CSS', 'Javascript', 'HTML'],
-    cardButton: '#',
+    description: 'An interactive gallery that showcase my project',
+    githublink: '#',
+    liveserver: '#',
   },
   {
     image: '#',
     title: 'Multi-Post Stories Gain+Glory',
     technologies: ['Ruby on Rails', 'CSS', 'Javascript', 'HTML'],
-    cardButton: '#',
+    description: 'An interactive gallery that showcase my project',
+    githublink: '#',
+    liveserver:  '#',
   },
   {
     image: '#',
     title: 'Multi-Post Stories Gain+Glory',
     technologies: ['Ruby on Rails', 'CSS', 'Javascript', 'HTML'],
-    cardButton: '#',
+    description: 'An interactive gallery that showcase my project',
+    githublink: '#',
+    liveserver: '#',
   },
   {
-    image: '#',
+    image: './assets/image/Snapshoot Portfolio (2).svg',
     title: 'Multi-Post Stories Gain+Glory',
     technologies: ['Ruby on Rails', 'CSS', 'Javascript', 'HTML'],
-    cardButton: '#',
+    description: 'An interactive gallery that showcase my project',
+    githublink: '#',
+    liveserver: '#',
   },
   {
-    image: '#',
+    image: './assets/image/Snapshoot Portfolio (2).svg',
     title: 'Multi-Post Stories Gain+Glory',
     technologies: ['Ruby on Rails', 'CSS', 'Javascript', 'HTML'],
-    cardButton: '#',
+    description: 'An interactive gallery that showcase my project',
+    githublink: '#',
+    liveserver: '#',
+    
   },
 ];
 
@@ -66,9 +81,91 @@ function createProjectCard(project) {
   card.appendChild(cardList);
   const cardBtn = document.createElement('button');
   cardBtn.type = 'button';
-  cardBtn.innerHTML = 'See Project';
+  cardBtn.innerHTML = "See Project";
   card.appendChild(cardBtn);
+  cardBtn.addEventListener("click", () => openModal(project))
   return card;
+}
+
+function openModal(projects){
+  const modal = document.createElement("div");
+  modal.classList.add("modal_style");
+  modal.id = "projectModal";
+
+  const closeButton = document.createElement("span");
+  closeButton.classList.add("close_btn");
+  closeButton.innerHTML = "&times";
+  closeButton.addEventListener("click", closeModal);
+  modal_content.appendChild(closeButton);
+
+  const modal_content = document.createElement("div");
+  modal_content.classList.add("modal_content");
+  modal.appendChild(modal_content);
+
+  const modal_image = document.createElement("img");
+  modal_image.src = projects.image;
+  modal_image.alt = projects.title;
+  modal_content.appendChild(modal_image);
+
+  const modal_title = document.createElement("h1");
+  modal_title.innerHTML = projects.title
+  modal_content.appendChild(modal_title);
+
+  const modal_list = document.createElement("ul");
+  modal_list.id='modalTechnologies'
+  projects.technologies.forEach((element) => {
+    const listItem = document.createElement("li");
+    listItem.innerHTML = element
+    modal_list.appendChild(listItem);
+  })
+  modal_content.appendChild(modal_list);
+
+  const modal_links = document.createElement("div");
+  modal_links.classList.add("modal_links");
+
+  const modal_description = document.createElement("p");
+  modal_description.innerHTML = projects.description
+  modal_content.appendChild(modal_description);
+  modal.appendChild(modal_content);
+  document.body.appendChild(modal)
+
+  const githublink = document.createElement("a");
+  githublink.href = projects.githubLink;
+  githublink.target = "blank";
+  githublink.textContent = "Github Repo";
+  githublink.classList.add("button");
+  modal_links.appendChild(githublink);
+
+  const liveserver = document.createElement("a");
+  liveserver.href = projects.liveserver;
+  liveserver.target = "blank";
+  liveserver.textContent = "Live Site"
+  liveserver.classList.add("button");
+  modal_links.appendChild(liveserver);
+
+  modal_content.appendChild(modal_links);
+  modal.appendChild(modal_content);
+  document.body.appendChild(modal);
+
+  window.addEventListener("click", outsideClick);
+
+  }
+
+  //Function to close modal
+  function closeModal(){
+    const modal = document.getElementById("projectModal");
+    if (modal) {
+      modal.remove();
+      window.removeEventListener("click", outsideClick);
+    }
+  }
+
+  //Close modal when clicking outside modal content
+  function outsideClick(event){
+  const modal = document.getElementById("projectModal");
+  if(event.target === modal){
+  closeModal();
+  }
 }
 
 // Function to insert the cards in the projects_cards div
